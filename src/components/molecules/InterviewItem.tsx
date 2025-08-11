cb945c974d87b5f20f0b71bf84794e32ddaf431e
ggalmury <body0922@naver.com>
2025-08-11 19:37:28 +0900

    refactor: 인터뷰 섹션 리팩토링

--- src/components/molecules/InterviewItem.tsx
+++ src/components/molecules/InterviewItem.tsx
@@ -1,17 +1,18 @@
+import Interview from "@/types/interview";
+
 import Body1 from "@/components/atoms/typography/Body1";
 import Heading1 from "@/components/atoms/typography/Heading1";
 
 interface InterviewItemProps {
-  question: string;
-  answer: string;
+  interview: Interview;
 }
 
-const InterviewItem = ({ question, answer }: InterviewItemProps) => {
+const InterviewItem = ({ interview }: InterviewItemProps) => {
   return (
-    <div className="flex flex-col px-4 py-2 gap-8 border-l-4 border-primary-600">
-      <Heading1 text={`Q. ${question}`} styles={{ color: "text-primary-600" }} />
+    <div className="flex flex-col px-4 py-2 gap-6 border-l-2 border-primary-600">
+      <Heading1 text={`Q. ${interview.question}`} styles={{ color: "text-primary-600" }} />
 
-      <Body1 text={answer} />
+      <Body1 text={interview.answer} />
     </div>
   );
 };
