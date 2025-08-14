be3947adaa60376e159566651d6eed8d3bf4552c
ggalmury <body0922@naver.com>
2025-08-14 20:30:43 +0900

    feat: 폰트 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,17 @@
 import { ReactNode } from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import clsx from "clsx";
 
 import "react-toastify/dist/ReactToastify.css";
 import "@/styles/global.css";
+
+import pretendard from "@/fonts/pretendard";
 
 import ToastProvider from "@/providers/ToastProvider";
 
 import Footer from "@/components/layouts/Footer";
 import Caption1 from "@/components/atoms/typography/Caption1";
 import ScrollToTopButton from "@/components/atoms/buttons/ScrollToTopButton";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "이재윤 | 프론트엔드 개발자",
@@ -34,7 +25,7 @@ interface RootLayoutProps {
 const RootLayout = ({ children }: Readonly<RootLayoutProps>) => {
   return (
     <html lang="ko">
-      <body className={clsx("antialiased", geistSans.variable, geistMono.variable)}>
+      <body className={clsx("antialiased", pretendard.variable)}>
         <ToastProvider />
 
         {children}
