e8d39661bdbedd4e7ccf17046e6c75d6d7e9b8ee
ggalmury <body0922@naver.com>
2025-08-14 19:26:12 +0900

    chore: 푸터 margin 조정

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -5,7 +5,7 @@ interface FooterProps {
 }
 
 const Footer = ({ children }: FooterProps) => {
-  return <footer className="flex items-center justify-center w-full h-28">{children}</footer>;
+  return <footer className="flex items-center justify-center w-full h-28 mt-24">{children}</footer>;
 };
 
 export default Footer;
